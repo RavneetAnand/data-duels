f7076c553bcb591ad3c98084b37e0f45ef6cebc3
Ravneet Anand <anand.ravneet099@gmail.com>
2024-02-27 14:54:39 +0000

    Improve hook error handling.

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -12,22 +12,17 @@ export const useFetch = ({ url, options, params }: FetchProps) => {
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-    try {
-      fetch(url, {
-        method: options?.method || "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(params),
-      })
-        .then((res) => res.json())
-        .then((data) => setData(data));
-
-      setIsLoading(false);
-    } catch (e) {
-      setError(e as Error);
-      setIsLoading(false);
-    }
+    fetch(url, {
+      method: options?.method || "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((e) => setError(e))
+      .finally(() => setIsLoading(false));
   }, [url]);
 
   return { data, isLoading, error };
